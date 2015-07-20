07f764df40ddd6d48dcb532a730ff3b5f10a8594
Chris Zuber <shgysk8zer0@gmail.com>
2015-07-19 17:04:46 -0700

    Updated so that request accepts an oAuth object in constructor
    * oAuth is handled externally.
    * `__invoke` takes an optional array of data for the request.
    * Do some basic error checking to see that the response parses
    correctly.
    * Added/updated documentation

--- requests/usertimeline.php
+++ requests/usertimeline.php
@@ -17,19 +17,38 @@ use \shgysk8zer0\Core_API as API;
  */
 final class UserTimeline extends TwitterAPI\Abstracts\Request implements API\Interfaces\cURL
 {
-	private $_oAuth = '';
+	const ENDPOINT = '/statuses/user_timeline.json';
 
-	public function __construct($key, $secret)
+	/**
+	 * The Twitter oAuth token object
+	 *
+	 * @var \shgysk8zer0\TwitterAPI\oAuth
+	 */
+	private $_oAuth = null;
+
+	/**
+	 * Create a new UserTimeline instance from a Twitter oAuth token object
+	 *
+	 * @param shgysk8zer0\TwitterAPI\oAuth $oAuth Parsed response from 'oauth2/token'
+	 */
+	public function __construct(TwitterAPI\oAuth $oAuth)
 	{
-		$this->_oAuth = new TwitterAPI\oAuth($key, $secret);
+		$this->_oAuth = $oAuth;
 	}
 
-	public function __invoke()
+	/**
+	 * Make the API request and parse the response
+	 *
+	 * @param  array  $data Optional array of data to use for the request ['screen_name' => ...]
+	 * @return \stdClass    JSON decoded Twiter response
+	 */
+	public function __invoke(array $data = array())
 	{
+		array_map([$this, '__set'], array_keys($data), array_values($data));
 		if (! (isset($this->screen_name) or isset($this->user_id))) {
 			throw new \Exception(sprintf('%s requires screen_name or user_id to be set', __METHOD__), 400);
 		}
-		$url = self::URI . self::VERSION . "/statuses/user_timeline.json";
+		$url = self::URI . self::VERSION . self::ENDPOINT;
 		$url .= '?' . http_build_query($this->{self::MAGIC_PROPERTY});
 		$this->curlInit($url);
 		$this->curlSetOpt(CURLOPT_RETURNTRANSFER, true);
@@ -37,8 +56,15 @@ final class UserTimeline extends TwitterAPI\Abstracts\Request implements API\Int
 			"Authorization: {$this->_oAuth}",
 			'User-Agent' => self::UA_STRING
 		]);
-		$resp = $this->curlExec();
-		$this->curlClose();
-		return json_decode($resp);
+		$resp = json_decode($this->curlExec());
+		if (empty($resp)) {
+			throw new \Exception('Unable to parse Twitter API response', 500);
+		} elseif (isset($resp->errors) and is_array($resp->errors) and ! empty($resp->errors)) {
+			$error = current($resp->errors);
+			throw new \Exception($error->message, $error->code);
+		} else {
+			$this->curlClose();
+			return $resp;
+		}
 	}
 }
